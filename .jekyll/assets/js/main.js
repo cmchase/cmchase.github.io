e2b33dc79385f5dcdae727183375fa0b3cae45c1
Cory Chase <cmchase@gmail.com>
2013-08-21 21:14:56 -0400

    Home page about content and mugshot.

--- .jekyll/assets/js/main.js
+++ .jekyll/assets/js/main.js
@@ -5,7 +5,7 @@
       return Math.floor(Math.random() * (max - min + 1)) + min;
     };
     num = getRandomInt(1, 9);
-    $(".background.no-bg").css("background-image", "url(/assets/img/covers/cover-" + num + ".jpg)");
+    $(".background.no-cover").css("background-image", "url(/assets/img/covers/cover-" + num + ".jpg)");
     $("article.post-link").on("click", function() {
       return window.location = $(this).data("link");
     });
